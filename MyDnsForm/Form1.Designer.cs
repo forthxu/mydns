b218d9a99bffc0fd13169ce0a505c245de1b82a2
forthxu <forthxu@gmail.com>
2016-02-01 15:01:25 +0800

    update README

---
+++ MyDnsForm/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MyDnsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.txtDns = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.RichTextBox();
+            this.txtInfo = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "查询的域名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Dns服务器";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(84, 13);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(169, 21);
+            this.txtDomain.TabIndex = 1;
+            this.txtDomain.Text = "dreamlist.cc";
+            // 
+            // txtDns
+            // 
+            this.txtDns.Location = new System.Drawing.Point(84, 44);
+            this.txtDns.Name = "txtDns";
+            this.txtDns.Size = new System.Drawing.Size(169, 21);
+            this.txtDns.TabIndex = 2;
+            this.txtDns.Text = "223.6.6.6";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "查询类新";
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(318, 14);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(66, 20);
+            this.cbType.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(261, 41);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(123, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "查询";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(15, 90);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(579, 179);
+            this.txtContent.TabIndex = 5;
+            this.txtContent.Text = "";
+            this.txtContent.TextChanged += new System.EventHandler(this.txtContent_TextChanged);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(391, 14);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(203, 56);
+            this.txtInfo.TabIndex = 6;
+            this.txtInfo.Text = "";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 281);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDns);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Forthxu\'s DnsSearch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.TextBox txtDns;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox txtContent;
+        private System.Windows.Forms.RichTextBox txtInfo;
+    }
+}
+
---
+++ MyDnsForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MyDnsPackage;
+namespace MyDnsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Type myenum = typeof(QueryType);
+            Array ar= Enum.GetValues(myenum);
+           QueryType qtype=  (QueryType ) ar.GetValue(cbType.SelectedIndex);
+            MyDns mydns = new MyDns();
+            if (!mydns.Search(txtDomain.Text.Trim(), qtype, txtDns.Text.Trim(), null ))
+            {
+
+                MessageBox.Show(mydns.header.RCODE.ToString());
+                return;
+            }
+            txtInfo.Clear();
+            txtInfo.AppendText (string.Format ("回复记录数：{0}\n",mydns.header.ANCOUNT) );
+            txtInfo.AppendText(string.Format("回复额外记录数：{0}\n", mydns.header.ARCOUNT ));
+            txtInfo.AppendText(string.Format("回复权威记录数：{0}", mydns.header.NSCOUNT ));
+
+            txtContent.Clear();
+            foreach (MyDnsRecord item in mydns.record.Records)
+            {
+                txtContent.AppendText(item.QType.ToString() + "   " + item.RDDate.ToString()+"\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Type myenum = typeof(QueryType);
+            Array qt = Enum.GetNames(myenum);
+            foreach (var name in qt)
+                cbType.Items.Add(name);
+            cbType.SelectedIndex = 0;
+        }
+
+        private void txtContent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
