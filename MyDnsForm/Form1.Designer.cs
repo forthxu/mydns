1fca43eb1ff4950fd9734ba4b125fa3d4570ac05
forthxu <forthxu@gmail.com>
2016-02-02 16:07:55 +0800

    update 发布下载

--- MyDnsForm/Form1.Designer.cs
+++ MyDnsForm/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtDomain = new System.Windows.Forms.TextBox();
@@ -132,6 +133,7 @@
             this.Controls.Add(this.txtDomain);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Forthxu\'s DnsSearch";
             this.Load += new System.EventHandler(this.Form1_Load);
